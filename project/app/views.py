11e9fc4ec167f2a58ed9cd7108db2192c03665bc
mehak <mehakpatelattitude03@gmail.com>
2024-04-29 15:17:44 +0530

    first commit in generic rest api

---
+++ project/app/views.py
@@ -0,0 +1,14 @@
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from django.shortcuts import render
+
+
+class StudentList(generics.ListCreateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
